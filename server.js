85e9b38ac11213b4ee2ff715582cbf5d41f8f148
Afnan267 <m.afnanakber@gmail.com>
2025-05-04 03:58:17 +0500

    feat: implement real time clock on server side using socket.io

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const http = require('http')
+const socketIo = require('socket.io')
+
+// Setup Server
+const app = express()
+const server = http.createServer(app)
+const io = socketIo(server)
+
+// Serve static files
+app.use(express.static('public'));
+
+io.on("connection", (socket) =>{
+    console.log("A user connected");
+
+    // Emit current time every second
+    setInterval(()=>{
+        const now = new Date().toLocaleTimeString();
+        socket.emit("time", now)
+    }, 1000)
+
+    socket.on("disconnect", () => {
+        console.log("A user disconnected");
+    });
+});
+
+server.listen(3000, ()=>{
+    console.log("Server is running on http://localhost:3000");
+});
